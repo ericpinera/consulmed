06197c80f1811530dfc8cdad25ff8589e60d4904
ericpinera <eepinera.emp@gmail.com>
2019-08-06 08:20:03 -0500

    Aplicación principal

--- pacientes/views.py
+++ pacientes/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def vista_pacientes(request):
     contexto ={}
-    return render(request, "pacientes/vista_pacientes.html/", contexto)
+    return render(request, "vista_pacientes.html", contexto)
